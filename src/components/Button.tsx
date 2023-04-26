cfc54d7bafe1b4b876673ac3df308e5d96c340c4
Marina Portugal <marinapsvreis@gmail.com>
2023-04-26 20:55:27 -0300

    :sparkles: Estrutura do projeto com StyledComponents, ThemeProvider e Eslint configurados

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, ButtonVariant } from './Button.styles'
+
+interface ButtonContainerProps {
+  variant?: ButtonVariant
+}
+
+export function Button({ variant = 'primary' }: ButtonContainerProps) {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
